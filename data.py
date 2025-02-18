196d67fcfaa249b0702d3795a7c3be1dc02c48d5
Grsneed <gino.sneed@gmail.com>
2025-02-17 19:32:10 -0500

    1st commit

--- data.py
+++ data.py
@@ -1,1 +1,8 @@
 # File with constants
+URBAN_ROUTES_URL = ' https://cnt-4512417b-387c-4fa9-9453-63c5b307dc92.containerhub.tripleten-services.com'
+ADDRESS_FROM = 'East 2nd Street. 601'
+ADDRESS_TO = '1300 1st ST'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
--- main.py
+++ main.py
@@ -1,2 +1,53 @@
+import data
+import helpers
+from data import URBAN_ROUTES_URL
+
 
 class TestUrbanRoutes:
+    @classmethod
+    def setup_class(cls):
+        if  helpers.is_url_reachable(URBAN_ROUTES_URL):
+            print("Connected to the Urban Routes Server")
+        else:
+            print("Cannot connect to Urban Routes")
+
+    def test_set_route(self):
+        # Add in S8
+        print("function created for set route")
+    pass
+
+    def test_select_plan(self):
+        # Add in S8
+        print("function created for select plan ")
+    pass
+
+    def test_fill_phone_number(self):
+        # Add in S8
+        print("function created for fill phone number ")
+    pass
+
+    def test_fill_card(self):
+        # Add in S8
+        print("function created for fill card")
+    pass
+
+    def test_comment_for_driver(self):
+        # Add in S8
+        print("function created for comment for driver")
+    pass
+
+    def test_order_blanket_and_handkerchiefs(self):
+        # Add in S8
+        print("function created for comment for driver")
+    pass
+
+    def test_order_2_ice_creams(self):
+        for i in range(2):
+    # Add in S8
+            print("function created for order 2 ice cream")
+    pass
+
+    def test_car_search_model_appears(self):
+        # Add in S8
+        print("function created for car search model appears")
+    pass
