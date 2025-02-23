fe037e44804528408c5a58d61f249cd94adafd56
Grsneed <gino.sneed@gmail.com>
2025-02-23 11:48:59 -0500

    1st commit

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 # File with constants
-URBAN_ROUTES_URL = '  https://cnt-5aa68186-8563-44dd-a218-8b9e0f7d28ae.containerhub.tripleten-services.com'
+URBAN_ROUTES_URL = '   https://cnt-94b8ee6b-edaa-4691-9189-d860a5e97fee.containerhub.tripleten-services.com'
 ADDRESS_FROM = 'East 2nd Street. 601'
 ADDRESS_TO = '1300 1st ST'
 PHONE_NUMBER = '+1 123 123 12 12'
--- main.py
+++ main.py
@@ -16,41 +16,41 @@ class TestUrbanRoutes:
     def test_set_route(self):
         # Add in S8
         print("function created for set route")
-    pass
+        pass
 
     def test_select_plan(self):
         # Add in S8
         print("function created for select plan ")
-    pass
+        pass
 
     def test_fill_phone_number(self):
         # Add in S8
         print("function created for fill phone number ")
-    pass
+        pass
 
     def test_fill_card(self):
         # Add in S8
         print("function created for fill card")
-    pass
+        pass
 
     def test_comment_for_driver(self):
         # Add in S8
         print("function created for comment for driver")
-    pass
+        pass
 
     def test_order_blanket_and_handkerchiefs(self):
         # Add in S8
         print("function created for comment for driver")
-    pass
+        pass
 
     def test_order_2_ice_creams(self):
         for i in range(2):
-    # Add in S8
-    #Adding a loop to iterate twice for order_2_ice_cream
+            # Add in S8
+            #Adding a loop to iterate twice for order_2_ice_cream
             print("function created for order 2 ice cream")
-    pass
+            pass
 
     def test_car_search_model_appears(self):
         # Add in S8
         print("function created for car search model appears")
-    pass
+        pass
